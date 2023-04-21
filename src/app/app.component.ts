8354d3d80ef5c49a1e308f7259e12bb2101cfc2c
Richardson94 <richcori94@gmail.com>
2023-04-20 22:27:52 -0400

    adding modules into the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils/analytics.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'Web-greenhouse-Angular';
+export class AppComponent implements OnInit {
+  constructor(private analytics: AnalyticsService) {}
+
+  ngOnInit() {
+    this.analytics.trackPageViews();
+  }
 }
